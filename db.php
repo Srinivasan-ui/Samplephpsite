1bafc2f89eb4743a365cadfa418b647b2224f66b
Srinivasan <srinivasan.uiux@gmail.com>
2025-09-20 16:47:04 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,17 @@
 <?php
-$servername = "sqlXXX.epizy.com"; // InfinityFree panel la irukum host name
-$username = "epiz_12345678"; // panel la kudutha username
-$password = "yourpassword";  // panel la set panna password
-$dbname = "epiz_12345678_testdb"; // panel la kudutha db name
+$servername = "gateway01.ap-southeast-1.prod.aws.tidbcloud.com"; // host
+$username = "4Xa7iRxUy3mSekr.root";                               // username
+$password = "ZjPDz7cHD6n3SGDQ";                                   // password
+$dbname = "myappdb";                                              // database
+$port = 4000; // TiDB Cloud default MySQL port (if 4000, check dashboard)
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Enable SSL connection (TiDB / Planetscale require SSL)
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+mysqli_real_connect($conn, $servername, $username, $password, $dbname, $port, NULL, MYSQLI_CLIENT_SSL);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// Test connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
 ?>
